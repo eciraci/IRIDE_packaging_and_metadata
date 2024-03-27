85946553ff75db8703006149da0158077a502eb3
Enrico Ciraci <eciraci@uci.edu>
2024-03-27 08:55:26 +0100

    - Several Changes to concatenate bursts with different missing dates.

--- list_bucket_content.py
+++ list_bucket_content.py
@@ -1,6 +1,9 @@
 """
-Written by Enrico Ciracì - March 2024
+Written by Enrico Ciraci' - March 2024
 Create a report of the content of an S3 bucket.
+
+NOTE: This script has been tested only with the first release of IRIDE
+    which is based only on Sentinel-1 data.
 
 Python Dependencies:
 - pandas:Python Data Analysis Library
@@ -20,6 +23,134 @@ from iride_utils.aoi_info import get_aoi_info
 from iride_utils.gsp_description import gsp_d_type, gsp_description
 
 SENSOR = 'SNT'    # - Sentinel-1
+
+
+def get_gsp_direction(processing: str, svc_id: str, gsp_id: str) -> str:
+    """
+    Get the direction of the GSP from the processing string.
+    At the moment, does not include SE-S3-07
+    Args:
+        processing: processing field as a string
+        svc_id: svc id
+        gsp_id: gsp id
+    Returns: Directions value as a string
+    """
+    if svc_id == "SE-S3-01":
+        if gsp_id in [f"S3-01-{SENSOR}-03", f"S3-01-{SENSOR}-04"]:
+            return processing[3]
+        else:
+            return processing[6]
+
+    elif svc_id == "SE-S3-02":
+        if len(processing) == 5:
+            return processing[4]
+        else:
+            return processing[6]
+
+    elif svc_id == "SE-S3-03":
+        if len(processing) > 13:
+            return processing[9]
+        else:
+            return processing[6]
+
+    elif svc_id == "SE-S3-04":
+        return processing[6]
+
+    elif svc_id == "SE-S3-05":
+        if len(processing) >= 11:
+            return processing[9]
+        return processing[6]
+
+    elif svc_id == "SE-S3-06":
+        if len(processing) >= 14:
+            return processing[9]
+        if len(processing) == 13:
+            return processing[6]
+        if len(processing) <= 8:
+            return 'NA'
+        return processing[6]
+
+    else:
+        return "NA"
+
+
+def get_calibrated_gsp_id(processing: str, svc_id: str, gsp_id: str) -> str:
+    """
+    Get the calibrated GSP ID
+    Args:
+        processing: processing field as a string
+        svc_id: svc id
+        gsp_id: gsp id
+
+    Returns:
+       Calibration Flag as a string
+    """
+    if svc_id == "SE-S3-01":
+        if gsp_id in [f"S3-01-{SENSOR}-02", f"S3-01-{SENSOR}-03"]:
+            return 'Yes'
+        elif gsp_id in [f"S3-01-{SENSOR}-04"]:
+            return 'Yes'
+        else:
+            return 'No'
+
+    if svc_id == "SE-S3-02":
+        if len(processing) == 5:
+            return 'Yes'
+        else:
+            if processing.endswith("C"):
+                return 'Yes'
+            else:
+                return 'No'
+
+    if svc_id == "SE-S3-03":
+        if len(processing) == 9:
+            calib = processing[7]
+        else:
+            if gsp_id == "S3-03-CHA-04":
+                calib = processing[7]
+            else:
+                calib = processing[10]
+
+        if calib in ["O", "C"]:
+            return 'Yes'
+        else:
+            return 'No'
+
+    if svc_id == "SE-S3-04":
+        return 'No'
+    if svc_id == "SE-S3-05":
+        if len(processing) <= 10:
+            calib = processing[7]
+        else:
+            calib = processing[10]
+
+        if calib in ["M", "C"]:
+            return 'Yes'
+        else:
+            return 'No'
+
+    if svc_id == "SE-S3-06":
+        if len(processing) == 8:
+            calib = processing[6]
+        elif len(processing) == 9:
+            calib = processing[7]
+        elif len(processing) == 11:
+            calib = processing[7]
+        elif len(processing) == 13:
+            calib = processing[7]
+        else:
+            calib = processing[10]
+
+        if calib in ["O", "C"]:
+            return 'Yes'
+        else:
+            return 'No'
+
+    if svc_id == "SE-S3-07":
+        # - This operation will need to be updated after the first release
+        # - of this product.
+        return 'NA'
+    return 'NA'
 
 
 def main() -> None:
@@ -59,6 +190,8 @@ def main() -> None:
     gsp_path_list = []
     start_date_list = []
     end_date_list = []
+    o_dir = []
+    calib_list = []
     s_delivery_date_list = []
     delivery_date_list = []
     sensor_list = []
@@ -66,7 +199,8 @@ def main() -> None:
     d_type_list = []
 
     # - To skip - suffixes of filese to skip
-    skip_suffixes = ["DS_Store", "qml", "sld", "/"]
+    skip_suffixes = ["DS_Store", "qml", "sld",
+                     ".DS_Store", ".qml", ".sld", "/"]
 
     for key in conn.list_objects(Bucket=args.bucket_name)['Contents']:
         if args.sub_dir is not None and args.sub_dir in key['Key']:
@@ -86,7 +220,9 @@ def main() -> None:
             sensor = key_name_list[2]
             aoi_id = key_name_list[4]
             gsp_id = key_name_list[5]
+            prod_id = key_name_list[-1]
             gsp_path = key_name
+
             # - Extract the start and end date and convert to datetime
             start_time_str = key_name_list[3].split("_")[0]
             start_date = (datetime(int(start_time_str[0:4]),
@@ -99,17 +235,27 @@ def main() -> None:
                                  15)
                         .strftime("%d/%m/%Y"))
 
-            if svc_id == "SE-S3-01":
-                # - Set the delivery date
-                d_date = date(2024, 2, 15)
-            else:
-                d_date = date(2024, 3, 1)
+            # - For this first release use only the 15th of March 2024
+            d_date = date(2024, 3, 15)
             delivery_date = d_date.strftime("%d/%m/%Y")
+
+            # - Extract 'Processing' field from the key
+            processing = prod_id.split("_")[4]
+
+            # - Get the direction of the GSP
+            d_dir = get_gsp_direction(processing, svc_id, gsp_id)
+            # - Get the calibrated flag
+            calib = get_calibrated_gsp_id(processing, svc_id, gsp_id)
+            if gsp_id == "S3-01-SNT-04":
+                print(calib)
 
             # - Append the elements to the lists
             svc_id_list.append(svc_id)
             gsp_id_list.append(gsp_id)
             gsp_path_list.append(gsp_path)
+            o_dir.append(d_dir)
+            calib_list.append(calib)
+
             try:
                 aoi_list.append(get_aoi_info(aoi_id)['aoi_name'])
             except ValueError:
@@ -137,6 +283,8 @@ def main() -> None:
     gsp_df["Delivery_Date"] = delivery_date_list
     gsp_df["Scheduled_Delivery_Date"] = s_delivery_date_list
     gsp_df["Sensor"] = sensor_list
+    gsp_df["Direction"] = o_dir
+    gsp_df["Calibrated"] = calib_list
     gsp_df["Data_Type"] = d_type_list
     gsp_df["GSP_Name"] = name_list
 
